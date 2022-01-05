780578170d1473e4fc521ddea23b7c86ead2ce9f
hamadkhan59 <86347085+hamadkhan59@users.noreply.github.com>
2022-01-05 20:20:16 +0500

    Master Commit

---
+++ BioMetrixCore/AttendanceForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BioMetrixCore
+{
+    partial class AttendanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStaff = new System.Windows.Forms.Label();
+            this.lblStaffIdValue = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStaff
+            // 
+            this.lblStaff.AutoSize = true;
+            this.lblStaff.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaff.Location = new System.Drawing.Point(59, 23);
+            this.lblStaff.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStaff.Name = "lblStaff";
+            this.lblStaff.Size = new System.Drawing.Size(111, 32);
+            this.lblStaff.TabIndex = 0;
+            this.lblStaff.Text = "Staff Id :";
+            // 
+            // lblStaffIdValue
+            // 
+            this.lblStaffIdValue.AutoSize = true;
+            this.lblStaffIdValue.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffIdValue.Location = new System.Drawing.Point(163, 23);
+            this.lblStaffIdValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStaffIdValue.Name = "lblStaffIdValue";
+            this.lblStaffIdValue.Size = new System.Drawing.Size(166, 32);
+            this.lblStaffIdValue.TabIndex = 1;
+            this.lblStaffIdValue.Text = "Staff Id Value";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Red;
+            this.lblTime.Location = new System.Drawing.Point(396, 23);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(71, 32);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "Time";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(47, 86);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1312, 846);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AttendanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1410, 978);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblStaffIdValue);
+            this.Controls.Add(this.lblStaff);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "AttendanceForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Attendance Success";
+            this.Load += new System.EventHandler(this.AttendanceForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStaff;
+        private System.Windows.Forms.Label lblStaffIdValue;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
